b34c8e879687526746887616d8f6f95ebdaaebbd
Oriolgresa <07uri07@gmail.com>
2016-10-06 15:20:28 +0200

    commit

--- EjemploSocket/src/main/java/edu/upc/eetac/dsa/Cliente.java
+++ EjemploSocket/src/main/java/edu/upc/eetac/dsa/Cliente.java
@@ -4,4 +4,5 @@ package edu.upc.eetac.dsa;
  * Created by OriolGresa on 28/9/16.
  */
 public class Cliente {
+
 }
--- Figures/src/main/java/edu/upc/eetac/dsa/Figure.java
+++ Figures/src/main/java/edu/upc/eetac/dsa/Figure.java
@@ -4,5 +4,5 @@ package edu.upc.eetac.dsa;
  * Created by OriolGresa on 15/9/16.
  */
 public interface Figure {
-    double area ();
+    double area();
 }
